7985576421f518790920aeb1e8ed1f39acdf34da
sdphat <77732077+sdphat@users.noreply.github.com>
2023-12-30 22:24:41 +0700

    Styling invitation page

--- src/routes/Invitation/InvitationPage.tsx
+++ src/routes/Invitation/InvitationPage.tsx
@@ -1,20 +1,23 @@
+import { HttpStatusCode } from "axios";
+import { useEffect, useState } from "react";
 import { useMutation } from "react-query";
 import {
   createSearchParams,
-  useHref,
   useLocation,
   useNavigate,
   useSearchParams,
 } from "react-router-dom";
+import { MoonLoader } from "react-spinners";
 import axiosInstance from "../../api/axios";
-import { useEffect } from "react";
-import { HttpStatusCode } from "axios";
+import AppIcon from "../../assets/app-icon-with-text.svg";
 import ROUTES from "../../constants/routes";
 
 const InvitationPage = () => {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
   const location = useLocation();
+  const [isLoading, setLoading] = useState(true);
+  const [isValid, setValid] = useState(false);
 
   const codeMutation = useMutation({
     mutationFn: async (code: string) => {
@@ -24,8 +27,11 @@ const InvitationPage = () => {
           code,
         }
       );
+
+      setLoading(false);
+
       if (response.status === HttpStatusCode.BadRequest) {
-        console.log("Code is invalid");
+        setValid(false);
         return;
       }
       if (response.status === HttpStatusCode.NotFound) {
@@ -54,7 +60,26 @@ const InvitationPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <div>InvitationPage</div>;
+  return (
+    <div className="grid place-items-center my-12">
+      <img src={AppIcon} className="h-16 mb-16" />
+      {isLoading && (
+        <>
+          <MoonLoader color="#1d4ed8" />
+          <div className="mt-2 font-medium">
+            Please wait. We are currently processing your invitation.
+          </div>
+        </>
+      )}
+
+      {!isValid && (
+        <div className="mt-2 font-medium">
+          Your invitation code is either incorrect or expired. Please ask your
+          board admins to resend the code.
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default InvitationPage;
