ecfc3a827a2c3d23cbce743303b7e9107fc32947
sdphat <77732077+sdphat@users.noreply.github.com>
2024-01-08 15:07:09 +0700

    Fix modal doesn't cover entire screen

--- src/components/ModalContainer.tsx
+++ src/components/ModalContainer.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { createPortal } from "react-dom";
 
 const ModalContainer = ({
   children,
@@ -7,7 +8,7 @@ const ModalContainer = ({
   children?: ReactNode;
   onClose: () => void;
 }) => {
-  return (
+  return createPortal(
     <div
       className="absolute left-0 top-0 w-screen h-screen overflow-auto flex justify-center py-24 bg-black bg-opacity-60"
       onClick={(e) => {
@@ -17,7 +18,8 @@ const ModalContainer = ({
       }}
     >
       {children}
-    </div>
+    </div>,
+    document.body
   );
 };
 
